953b08a5a738ac007f6674057e9270b5884bb2a5
rauljordan <raul@prysmaticlabs.com>
2020-06-30 16:18:28 -0500

    add petname encoding

--- validator/keymanager/v2/direct/direct.go
+++ validator/keymanager/v2/direct/direct.go
@@ -2,13 +2,14 @@ package direct
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 
+	petname "github.com/dustinkirkland/golang-petname"
 	"github.com/pkg/errors"
-
+	"github.com/prysmaticlabs/prysm/shared/bls"
 	"github.com/sirupsen/logrus"
 	keystorev4 "github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4"
-
-	"github.com/prysmaticlabs/prysm/shared/bls"
 )
 
 var log = logrus.WithField("prefix", "keymanager-v2")
@@ -36,16 +37,23 @@ func (dr *Keymanager) CreateAccount(ctx context.Context, password string) error 
 	}
 	encryptor := keystorev4.New()
 	signingKey := bls.RandKey()
-	// Withdrawal key will be shown by using a mnemonic.
-	withdrawalKey := bls.RandKey()
 	keystore, err := encryptor.Encrypt(signingKey.Marshal(), []byte(password))
 	if err != nil {
 		return errors.Wrap(err, "could not encrypt signing key")
 	}
+	encodedAccount, err := json.Marshal(keystore)
+	if err != nil {
+		return errors.Wrap(err, "could not encode keystore.json file")
+	}
+	accountName := petname.Generate(3, "-")
 	return errors.New("unimplemented")
 }
 
 // ConfigFile returns a marshaled configuration file for a direct keymanager.
 func (dr *Keymanager) ConfigFile(ctx context.Context) ([]byte, error) {
 	return nil, nil
+}
+
+func (dr *Keymanager) writeAccountToFile(ctx context.Context, w io.Writer) {
+
 }
